14040635e01e6b6be9366977f141dc5a78f00e96
Hex <hex@codeigniter.org.cn>
2026-09-17 12:48:50 +0800

    build(qoderwork): 🚀 发布 0.4.5
    * 版本管理: 将 qoderwork/VERSION 升级至 0.4.5，同步 main.go 中的 version 变量。
    * 本次发布内容: 修复耗尽账号面板进度条误显加载中问题。

--- qoderwork/main.go
+++ qoderwork/main.go
@@ -332,7 +332,7 @@ type registrationCapability struct {
 }
 
 // version is injected at build time via -ldflags "-X main.version=...".
-var version = "0.4.4"
+var version = "0.4.5"
 
 func wbRegistration() registration {
 	return registration{
