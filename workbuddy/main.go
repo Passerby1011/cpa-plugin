4edee3bbb5f27d0be71bd4c5b6a0b1e06a223199
Hex <hex@codeigniter.org.cn>
2026-09-18 21:03:27 +0800

    build(workbuddy): 🚀 发布 0.11.0

--- workbuddy/main.go
+++ workbuddy/main.go
@@ -349,7 +349,7 @@ type registrationCapability struct {
 }
 
 // version is injected at build time via -ldflags "-X main.version=...".
-var version = "0.10.1"
+var version = "0.11.0"
 
 func wbRegistration() registration {
 	return registration{
