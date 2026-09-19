53c52a4ad6ce6b3eb13547cda631c0bf96696312
Hex <hex@codeigniter.org.cn>
2026-09-20 00:42:09 +0800

    build(qoderwork): 🚀 发布 0.4.7

--- qoderwork/main.go
+++ qoderwork/main.go
@@ -332,7 +332,7 @@ type registrationCapability struct {
 }
 
 // version is injected at build time via -ldflags "-X main.version=...".
-var version = "0.4.6"
+var version = "0.4.7"
 
 func wbRegistration() registration {
 	return registration{
